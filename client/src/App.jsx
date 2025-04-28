d58cae1ccc3aef9a6b528428f6be1f01a31110d8
Son Nguyen <son18122001@gmail.com>
2025-04-28 17:47:50 +0700

    restore point 1 - 1:48:59

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import OTP from "./pages/OTP.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/otp-verification/:email" element={<OTP />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+      </Routes>
+      <ToastContainer theme={"dark"} />
+    </Router>
+  );
 };
 
 export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Sidebar from "../layout/SideBar.jsx";
+import UserDashboard from "../components/UserDashboard.jsx";
+import AdminDashboard from "../components/AdminDashboard.jsx";
+import BookManagement from "../components/BookManagement.jsx";
+import Catalog from "../components/Catalog.jsx";
+import Users from "../components/Users.jsx";
+import MyBorrowedBooks from "../components/MyBorrowedBooks.jsx";
 
 const Home = () => {
-  return <></>;
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [selectedComponent, setSelectedComponent] = useState("");
+
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
+
+  // if (!isAuthenticated) {
+  //   return <Navigate to={"/login"} />;
+  // }
+
+  return (
+    <>
+      <div className="relative md:pl-64 flex min-h-screen bg-gray-100">
+        <div
+          className="md:hidden z-10 absolute right-6 top-4 sm:top-6 flex justify-center items-center
+         bg-black rounded-md h-9 w-9 text-white"
+        >
+          <GiHamburgerMenu
+            className="text-2xl"
+            onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+          />
+        </div>
+        <Sidebar
+          isSideBarOpen={isSideBarOpen}
+          setIsSideBarOpen={setIsSideBarOpen}
+          setSelectedComponent={setSelectedComponent}
+        />
+
+        {(() => {
+          switch (selectedComponent) {
+            case "Dashboard":
+              return user?.role === "User" ? (
+                <UserDashboard />
+              ) : (
+                <AdminDashboard />
+              );
+              break;
+            case "Books":
+              return <BookManagement />;
+              break;
+            case "Catalog":
+              if (user.role === "Admin") {
+                return <Catalog />;
+              }
+              break;
+            case "Users":
+              if (user.role === "Admin") {
+                return <Users />;
+              }
+              break;
+            case "My Borrowed Books":
+              return <MyBorrowedBooks />;
+              break;
+            default:
+              return user?.role === "User" ? (
+                <UserDashboard />
+              ) : (
+                <AdminDashboard />
+              );
+              break;
+          }
+        })()}
+      </div>
+    </>
+  );
 };
 
 export default Home;
